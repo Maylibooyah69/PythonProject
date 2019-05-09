0fcfa1be72cc6993aebf6a7502bc8c5fa9126e11
m69 <maylibooyah69@gmail.com>
2019-05-08 17:09:42 -0700

    add output to a file

--- dscrape/template.py
+++ dscrape/template.py
@@ -7,7 +7,11 @@ Created on Wed May  8 15:57:44 2019
 """
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
 
+#params:
+outputfile='scraped/agoodmovietowatch.csv'
 quote_page = 'https://agoodmovietowatch.com/'
 page = urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
@@ -19,5 +23,12 @@ temp=[]
 for i in zip(name_box,name_box2):
     text="Title: "+i[0].text.strip() + "\nReview:" + i[1].text.strip()
     temp.append(text)
-print(temp[0])
+#print(temp[0])
 
+with open(outputfile,'a') as csv_file:
+    writer = csv.writer(csv_file)
+    for i in zip(name_box,name_box2):
+        t1="Title: "+i[0].text.strip()
+        t2="Review:" + i[1].text.strip()
+        writer.writerow([t1, t2,datetime.now()])
+print("hehe")
